4e00f376f2ccb0cc3a38181f7234d0106075ca63
oyerohabib <oyerohabib@yahoo.com>
2022-05-31 07:57:15 +0100

    completed windows object method and properties

---
+++ script-2.js
@@ -0,0 +1,71 @@
+var string = "hello world";
+var encodedString = btoa(string);
+var decodedString = atob(encodedString);
+
+const hello = document.getElementById("hello");
+// windows object methods
+// setInterval()
+// clearInterval()
+// setTimeout()
+// clearTimeout
+// blur()
+// alert()
+// close()
+
+// confirm()
+
+// var style = getComputedStyle(hello)
+// console.log(style);
+// console.log(getSelection(hello))
+
+// focus()
+// moveBy()
+// moveTo()
+// open()
+// print()
+// prompt()
+// requestAnimationFrame(func())
+// resizeBy(200, 400);
+// resizeTo(200, 400);
+// scrollBy(50);
+// scrollTo(50);
+// stop()
+
+// windows object properties
+
+// alert(closed)
+// alert(defaultStatus)
+
+// console.log(document)
+// console.log(frames)
+// console.log(history)
+// alert(innerHeight);
+// alert(innerWidth);
+// alert(length)
+// localStorage.setItem("username", "nasir");
+// alert(location)
+// alert(name)
+
+// console.log(navigator)
+
+// alert(opener)
+
+// console.log(parent)
+
+// console.log(self)
+
+// alert(outerHeight);
+// alert(outerWidth);
+
+// alert(pageXOffset); - scrollX
+// alert(pageYOffset) - scrollY
+
+// console.log(screen)
+// alert(screenLeft);  - screenX
+// alert(screenTop);  - screenY
+
+// sessionStorage.setItem("password", "habibo12345");
+
+// alert(status)
+
+// console.log(top)
