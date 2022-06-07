8e859db6827025252e206deb7aed51160b5bb7fe
oyerohabib <oyerohabib@yahoo.com>
2022-06-07 17:26:03 +0100

    restructured file system, dom event and properties

---
+++ script/script-3.js
@@ -0,0 +1,77 @@
+// dom events and properties
+function funcBlur() {
+  console.log("onblur");
+}
+
+function funcOnchange() {
+  console.log("onchange");
+}
+
+function funcOnfocus() {
+  console.log("focus");
+}
+
+function funcOnselect() {
+  console.log("selected")
+}
+
+function funcOnsubmit() {
+  console.log("submitted")
+}
+
+function funcOnreset() {
+  console.log("reset")
+}
+
+function funcOnkeydown() {
+  console.log("keydown")
+}
+
+// DIFFERENCE BETWEEN ONKEYDOWN AND ONKEYPRESS - 
+// ONKEYDOWN IS WHEN YOU PRESS ANY KEY
+// BUT ONKEYPRESS IS WHEN YOU PRESS A KEY THAT DISPLAYS ON THE INPUT FIELD
+
+function funcOnkeypress() {
+  console.log("keypress");
+}
+
+function funcOnkeyup() {
+  console.log("keyup");
+}
+
+
+function funcOnclick() {
+  console.log("onclick")
+}
+
+function funcOncontext() {
+  console.log("context menu")
+}
+
+function funcOndblclick() {
+  console.log("double click")
+}
+
+function onMouseEnter() {
+  console.log("mouse entered")
+}
+
+function onMouseLeave() {
+  console.log("mouse left");
+}
+
+function onMouseMove() {
+  console.log("mouse moving")
+}
+
+function onMouseDown() {
+  console.log("mouse down");
+}
+
+function onMouseOut() {
+  console.log("mouse out");
+}
+
+function funcAbort() {
+  alert("aborted")
+}
